2092e3f017c3f62e195157633423bbb809b59c97
명지대학교 컴퓨터공학과 백엔드 소프트웨어 개발 <mjubackend@gmail.com>
2024-10-14 23:49:53 +0900

    Create main.cpp

---
+++ lab13/header_vs_cpp/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+
+class SmartPointer {
+private:
+    int* ptr;
+
+public:
+    SmartPointer(int* p) : ptr{p} {}
+
+    ~SmartPointer() {
+        delete ptr;
+    }
+
+    int& operator*() {
+        return *ptr;
+    }
+
+    int* operator->() {
+        return ptr;
+    }
+
+    SmartPointer(const SmartPointer&) = delete;
+    SmartPointer& operator=(const SmartPointer&) = delete;
+
+    SmartPointer(SmartPointer&& sp) : ptr{sp.ptr} {
+        sp.ptr = nullptr;
+    }
+
+    SmartPointer& operator=(SmartPointer&& sp) {
+        if (this != &sp) {
+            delete ptr;
+            ptr = sp.ptr;
+            sp.ptr = nullptr;
+        }
+        return *this;
+    }
+};
+
+int main() {
+    SmartPointer sp(new int(42));
+
+    std::cout << "Value: " << *sp << "\n";
+
+    SmartPointer moved{move(sp)};
+
+    std::cout << "Value2: " << *moved << "\n";
+
+    return 0;
+}
