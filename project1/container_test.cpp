af24d5867cb936284463f457f2decd9f92d8414f
명지대학교 컴퓨터공학과 백엔드 소프트웨어 개발 <mjubackend@gmail.com>
2024-11-12 00:47:00 +0900

    Create container_test.cpp

---
+++ project1/container_test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "MyContainer.h"
+#include "MyVector.h"
+#include "MyList.h"
+
+using namespace std;
+
+void updateVectorElement(MyContainer &c, int update) {
+  MyVector &v = dynamic_cast<MyVector &>(c);
+  v[0] = update;
+  cout << v[0] << "\n";
+}
+
+void updateListElement(MyContainer &c, int update) {
+  MyList &l = dynamic_cast<MyList &>(c);
+  l.back() = update;
+  cout << l.back() << "\n";
+}
+
+int main() {
+  MyContainer *v = MyContainer::Create(MyContainer::VECTOR);
+
+  v->push_back(10);
+  cout << v->size() << "\n";
+  updateVectorElement(*v, 20);
+  updateListElement(*v, 30);
+  v->pop_back();
+
+  MyContainer *l = MyContainer::Create(MyContainer::LIST);
+  l->push_back(10);
+  cout << l->size() << "\n";
+  updateVectorElement(*l, 20);
+  updateListElement(*l, 30);
+  l->pop_back();
+}
